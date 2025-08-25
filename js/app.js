086cc8454a5ed4cfa1cd0b8c5120745839ace01f
macantomato <m.thomaeus@live.se>
2025-08-25 14:53:13 +0200

    small logic for js, and structure layour for html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
+function makeSet(name) {
+
+    const now = Date.now();  //current time
+
+    return {
+        id: newId(),
+        name: (typeof name === "string "),
+        createdAt: now,
+        updatedAt: "blank",
+        cards: []
+    }; 
+}
+
+function newId() {
+    const Id = Date.now();
+    return Id;
+}
+
 
